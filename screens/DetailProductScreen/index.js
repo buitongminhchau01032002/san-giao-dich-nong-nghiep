a80839fb63155bf9a47b60f09d3e2e16b0a16302
Minh Chau <buitongminhchau01032002@gmail.com>
2023-05-22 15:40:11 +0700

    💄 [detail product screen] completed detail product screen static ui

--- screens/DetailProductScreen/index.js
+++ screens/DetailProductScreen/index.js
@@ -1,9 +1,10 @@
-import { Button, HStack, NumberInputStepper, ScrollView, Text, View } from 'native-base';
+import { Button, Divider, HStack, Image, NumberInputStepper, ScrollView, Text, View } from 'native-base';
 import QuantityInput from './components/QuantityInput';
 import { useEffect, useState } from 'react';
 
 const PRODUCT = {
-    name: 'Hạt điều nâu cao cấp nhập khẩu',
+    name: 'Hạt điều nâu cao cấp nhập khẩu, chính hãng giá rẻ',
+    image: 'https://plus.unsplash.com/premium_photo-1663850873179-6a3ebbd8fe47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8ZnJ1aXR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
     category: {
         name: 'Trái cây',
     },
@@ -13,7 +14,9 @@ const PRODUCT = {
         scale: '100 ha',
         capicity: '1000 tấn',
     },
-    description: 'Sản phẩm chất lượng nhất thế giới',
+    quantity: 100,
+    description:
+        'Sản phẩm chất lượng nhất thế giới, Sản phẩm chất lượng nhất thế giới, Sản phẩm chất lượng nhất thế giới',
     minPurchase: 10,
     price: 100000,
     unit: 'gói',
@@ -27,18 +30,47 @@ function DetailProductScreen() {
         // TODO: call api
     }, []);
     return (
-        <View position="relative">
-            <ScrollView>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-                <Text pb={100}>fasdfasdfasdfsadfsad</Text>
-            </ScrollView>
+        <View position="relative" flex={1}>
+            <View pb="60">
+                <ScrollView px="3">
+                    <Image
+                        source={{ uri: product?.image }}
+                        alt=""
+                        mt="3"
+                        w="full"
+                        h="300"
+                        rounded="md"
+                        resizeMode="cover"
+                    />
+                    <Text fontSize="22" fontWeight="bold" mt="3">
+                        {product?.name}
+                    </Text>
+                    <Text fontSize="20" fontWeight="semibold" mt="2" color="primary.600">
+                        {product?.price + '₫ / ' + product?.unit}
+                    </Text>
+                    <Text
+                        mt="1"
+                        fontSize="16"
+                        color="gray.700"
+                        fontWeight="medium"
+                    >{`Còn lại: ${product?.quantity} ${product?.unit}`}</Text>
+                    <Divider my="2" />
+                    <Text fontSize="16" fontWeight="medium">
+                        Được cung cấp bởi:
+                    </Text>
+                    <Text mt="1" fontSize="18" color="primary.600" fontWeight="semibold">
+                        {product?.seller?.brandName}
+                    </Text>
+                    <Text mt="1" color="gray.700" fontWeight="medium">{`Địa chỉ: ${product?.seller?.address}`}</Text>
+                    <Text mt="1" color="gray.700" fontWeight="medium">{`Quy mô: ${product?.seller?.scale}`}</Text>
+                    <Text mt="1" color="gray.700" fontWeight="medium">{`Sản lượng: ${product?.seller?.capicity}`}</Text>
+                    <Divider my="2" />
+                    <Text fontSize="16" fontWeight="medium">
+                        Mô tả sản phẩm:
+                    </Text>
+                    <Text mt="1" color="gray.700">{`Sản lượng: ${product?.description}`}</Text>
+                </ScrollView>
+            </View>
 
             <HStack
                 justifyContent="space-between"
@@ -51,7 +83,12 @@ function DetailProductScreen() {
                 left="0"
                 right="0"
             >
-                <QuantityInput value={quantity} setValue={setQuantity} min={1} max={product?.minPurchase || 100} />
+                <QuantityInput
+                    value={quantity}
+                    setValue={setQuantity}
+                    min={product?.minPurchase || 0}
+                    max={product?.quantity || 100}
+                />
                 <Button>Thêm vào giỏ hàng</Button>
             </HStack>
         </View>
