c50050ab0f7e4ab1e04396582f04551a758f7023
DirhThi <20520771@gm.uit.edu.vn>
2023-07-15 09:11:23 +0700

    register seller

--- constants/Api.js
+++ constants/Api.js
@@ -1,2 +1,2 @@
-const API = 'https://b9bb-42-114-205-226.ngrok-free.app/api';
+const API = 'https://3a83-2001-ee0-260-9f7b-e58e-2d05-bfd6-cfc0.ngrok-free.app/api';
 export default API;
--- screens/UserScreens/SupplierRegister/RegisterScreen.js
+++ screens/UserScreens/SupplierRegister/RegisterScreen.js
@@ -1,28 +1,60 @@
-import { Button, Center, Text, Input, View, FormControl,ScrollView } from 'native-base';
+import { Button, Center, Text, Input, View, FormControl,ScrollView,Toast } from 'native-base';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
 import { useState } from 'react';
 import ImageInput from '../SellProduct/components/ImageInput';
+import { useSelector } from 'react-redux';
+import { userSelector } from '../../../redux/selectors/userSelector';
+import API from '../../../constants/Api';
+
 const validateScheme = Yup.object({
-    brand: Yup.string().required('Vui lòng điền tên thương hiệu'),
-    scale: Yup.number('Vui lòng nhập số').required('Vui lòng điền quy mô nhà vườn'),
+    brandName: Yup.string().required('Vui lòng điền tên thương hiệu'),
+    address: Yup.string().required('Vui lòng điền địa chỉ nhà vườn'),
+    scale: Yup.string().required('Vui lòng điền quy mô nhà vườn'),
     capicity: Yup.string().required('Vui lòng điền sản lượng cung cấp'),
     description: Yup.string().required('Vui lòng giới thiệu nhà vườn'),
-    image: Yup.string().required('Vui lòng thêm hình ảnh nhà vườn'),
+    imageSeller: Yup.string().required('Vui lòng thêm hình ảnh nhà vườn'),
 });
 
 const initFormValue = {
-    brand: '',
+    
+    brandName: '',
+    address:'',
     scale: '',
     capicity: '',
     description: '',
-    image: '',
+    imageSeller: '',
 };
 function SupplierRegisterScreen({ navigation }) {
     const [isValidateOnChange, setIsValidateOnChange] = useState(false);
     const [loading, setLoading] = useState(false);
-
-    async function handleSellerRegister(values) {}
+    const user=useSelector(userSelector)
+    async function handleSellerRegister(values) {
+        try {
+            setLoading(true);
+            const res = await fetch(`${API}/users/register-seller`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + user?.token,
+                },
+                body: JSON.stringify({...values,phone:user.phone}),
+            });
+            const data = await res.json();
+            if (data.error) {
+                Toast.show({ description: data.error.message });
+                console.log(data.error);
+                return;
+            }
+            Toast.show({ description: 'Đã trở thành nhà cung cấp!' });
+            navigation.goBack();
+        } catch (error) {
+            console.log(error);
+            Toast.show({ description: 'Có lỗi xảy ra!' });
+        } finally {
+            setLoading(false);
+        }
+    }
     return (
         <View flex={1}>
             <View
@@ -67,16 +99,29 @@ function SupplierRegisterScreen({ navigation }) {
                 {({ handleSubmit, handleChange, errors, values, validateForm, setFieldValue }) => (
                         <View p="4" m={4} borderRadius={10} bg="white">
                         <View pt={4} pl={4} pr={4} width={'full'}>
-                            <FormControl isRequired isInvalid={!!errors.brand}>
+                            <FormControl isRequired isInvalid={!!errors.brandName}>
                                 <FormControl.Label>Tên thương hiệu</FormControl.Label>
                                 <Input
                                     fontSize={18}
                                     fontWeight={'medium'}
                                     variant="underlined"
-                                    value={values.brand}
-                                    onChangeText={handleChange('brand')}
+                                    value={values.brandName}
+                                    onChangeText={handleChange('brandName')}
                                 />
-                                <FormControl.ErrorMessage>{errors.brand}</FormControl.ErrorMessage>
+                                <FormControl.ErrorMessage>{errors.brandName}</FormControl.ErrorMessage>
+                            </FormControl>
+                        </View>
+                        <View pt={4} pl={4} pr={4} width={'full'}>
+                            <FormControl isRequired isInvalid={!!errors.address}>
+                                <FormControl.Label>Địa chỉ nhà vườn</FormControl.Label>
+                                <Input
+                                    fontSize={18}
+                                    fontWeight={'medium'}
+                                    variant="underlined"
+                                    value={values.address}
+                                    onChangeText={handleChange('address')}
+                                />
+                                <FormControl.ErrorMessage>{errors.address}</FormControl.ErrorMessage>
                             </FormControl>
                         </View>
                         <View pt={4} pl={4} pr={4} width={'full'}>
@@ -121,14 +166,14 @@ function SupplierRegisterScreen({ navigation }) {
                         </View>
 
                         <View flexDirection={'row'} pt={4} pl={4} pr={4} width={'full'}>
-                        <FormControl isRequired isInvalid={!!errors.image} mt="4">
+                        <FormControl isRequired isInvalid={!!errors.imageSeller} mt="4">
                                 <FormControl.Label>Hình ảnh sản phẩm</FormControl.Label>
                                 <ImageInput
                                     mt="2"
-                                    initValue={values.image}
-                                    onChange={(image) => setFieldValue('image', image)}
+                                    initValue={values.imageSeller}
+                                    onChange={(imageSeller) => setFieldValue('imageSeller', imageSeller)}
                                 />
-                                <FormControl.ErrorMessage>{errors.image}</FormControl.ErrorMessage>
+                                <FormControl.ErrorMessage>{errors.imageSeller}</FormControl.ErrorMessage>
                             </FormControl>
                         </View>
                         <Button
